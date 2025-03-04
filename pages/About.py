c89ed412641cb08f161e5d9d9f3754cea772ddf1
Aqsa Gull <aqsa.gull.dev.ai99@gmail.com>
2025-03-04 13:44:02 +0500

    initialcommit

--- pages/About.py
+++ pages/About.py
@@ -7,9 +7,9 @@ st.set_page_config(page_title="About Us", page_icon="ℹ️", layout="wide")
 st.markdown("""
     <style>
         /* Background Styling */
-        # .stApp {
-        #     # background-color: #111111;
-        #     color: white;
+        .stApp {
+            background-color: #111111;
+            color: white;
         
         # }
         
